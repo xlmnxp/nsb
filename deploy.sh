ef44347623b3f787b0c89b2ccff0721727fe3c84
xlmnxp <xlmnxp@outlook.sa>
2017-06-26 08:28:23 +0300

    إضافة طريقة deploy جديدة

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+git config user.name "$USERNAME"
+git config user.email "$EMAIL"
+git init
+git remote add up "https://$GITHUB_TOKEN@github.com/$USERNAME/$REPO.git"
+git clone https://github.com/$USERNAME/$REPO.git
+git checkout -b gh-pages
+git rm -rf .
+rm -rf !(out|.git)
+cp -R ./out/. ./
+git add .
+rm -rf out
+git commit -am 'تدوينة جديدة'
+git push -f up gh-pages
