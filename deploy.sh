814c20fcc51dd525579333c98ce978abaccf1d37
xlmnxp <xlmnxp@outlook.sa>
2017-06-30 15:45:07 +0300

    إستخدام pull بدلًا عن clone

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git config user.name "$USERNAME"
 git config user.email "$EMAIL"
 git init
 git remote add up "https://$GITHUB_TOKEN@github.com/$USERNAME/$REPO.git"
-git clone https://github.com/$USERNAME/$REPO.git
+git pull
 git checkout -b gh-pages
 git rm -rf .
 rm -rf !\("out"|".git"|".gitignore"\)
